c95b2cd01ac1e649c416f1a75f4f5fbe2fad9f3e
Dan Wolf <daniel.john.wolf@gmail.com>
2021-05-01 21:37:31 -0700

    Compile and partially run

--- Function1.cs
+++ Function1.cs
@@ -12,6 +12,8 @@ using System.Net;
 using System.Net.Http.Headers;
 using Syncfusion.Pdf;
 using System.Collections.Specialized;
+using Newtonsoft.Json.Linq;
+using System;
 
 namespace pdfGenerator
 {
@@ -20,10 +22,9 @@ namespace pdfGenerator
         [FunctionName("pdfGenerator")]
         public static HttpResponseMessage Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, TraceWriter log, ExecutionContext executionContext)
         {
-
             string name = req.Query["url"];
             log.Error(name);
-            string[] splitUrl = name.Split('/');
+            string[] splitUrl = name.Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
 
             //Initialize HTML to PDF converter  
             HtmlToPdfConverter htmlConverter = new HtmlToPdfConverter();
@@ -37,40 +38,50 @@ namespace pdfGenerator
             //Load template based on requested client and requested template
             //var binDirectory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
             //var templatePath = System.IO.Path.Combine(binDirectory, "../", splitUrl[0], "/", splitUrl[1],".html");
-            var templatePath = System.IO.Path.Combine(executionContext.FunctionDirectory, "/", splitUrl[0], "/", splitUrl[1], ".html");
+            var templatePath = System.IO.Path.Combine(executionContext.FunctionDirectory, splitUrl[0], splitUrl[1]);
 
             var templateHtml = System.IO.File.ReadAllText(templatePath);
             
             log.Error(templateHtml);
 
+            JObject myJObject;
+
             //Grab POST body
 
-            using (StreamReader stream = new StreamReader(HttpContext.Request.Body))
+            using (StreamReader stream = new StreamReader(req.Body))
             {
                 string requestBody = stream.ReadToEnd();
+                myJObject = JObject.Parse(requestBody);
             }
 
             //Replace HTML values based on incoming JSON
-            templateHtml.Replace("{{letterFolio}}", myJObject.SelectToken("$.responseLetter.letterFolio").Value<string>()) 
+            templateHtml.Replace("{{letterFolio}}", myJObject.SelectToken("$.responseLetter.letterFolio").Value<string>());
             //Convert value replaced HTML to PDF
+
+            HttpResponseMessage response;
 
             //Convert URL to PDF 
             PdfDocument document = htmlConverter.Convert(templateHtml);
-            MemoryStream ms = new MemoryStream();
-            //Save the PDF document  
-            document.Save(ms);
-            ms.Position = 0;
 
-            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
-            response.Content = new ByteArrayContent(ms.ToArray());
+            using (MemoryStream ms = new MemoryStream())
+            {
+                //Save the PDF document  
+                document.Save(ms);
+                ms.Position = 0;
+
+                response = new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new ByteArrayContent(ms.ToArray())
+                };
+            }
+                
             response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
             {
                 FileName = "TBD.pdf"
             };
-            response.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/pdf");
-
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/pdf");
+            
             return response;
         }
-
     }
 }
